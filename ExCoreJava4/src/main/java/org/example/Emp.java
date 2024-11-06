23d37b8fac515b95eca95efa1801ed8e4d717a58
toilaminh <minh.luu.vkl@gmail.com>
2024-11-06 17:09:45 +0700

    Update

---
+++ ExCoreJava4/src/main/java/org/example/Emp.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Emp {
+    private static int index = 0;
+    private int EMP_INDEX;
+    private String ID;
+    private String NAME;
+    public Emp(String id, String name){
+        EMP_INDEX = index;
+        ID = id;
+        NAME = name;
+        index += 1;
+    }
+}
